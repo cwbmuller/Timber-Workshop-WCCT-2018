1486b1a1e00dddec704a325f37044f86cb00148f
Chris <chris@pango.world>
2017-10-26 09:20:08 +0200

    Update theme

--- themes/twentyseventeen-child/front-page.php
+++ themes/twentyseventeen-child/front-page.php
@@ -7,20 +7,29 @@
 
 if ( class_exists( 'Timber' ) ) {
 
+    // Set global context data
     $context = Timber::get_context();
+
+    // set specific post data to context
     $post = new TimberPost();
     $context['post'] = $post;
 
+    // Get latest blog posts
     $blog_args = array(
         'post_type' => 'post',
         'numberposts' => 3
     );
+
+    // set latest blog posts to context
     $context['blog_posts'] = Timber::get_posts($blog_args);
 
+    // Get featured products
     $product_args = array(
         'post_type' => 'product',
         'numberposts' => 3
     );
+
+    // Set featured products to context
     $context['products'] = Timber::get_posts($product_args);
 
     Timber::render(array('template-home.twig', 'page.twig'), $context);
--- themes/twentyseventeen-child/functions-timber.php
+++ themes/twentyseventeen-child/functions-timber.php
@@ -46,7 +46,7 @@ class StarterSite extends TimberSite {
 		$context['stuff'] = 'I am a value set in your functions.php file';
 		$context['notes'] = 'These values are available everytime you call Timber::get_context();';
 		$context['menu'] = new TimberMenu();
-//		$context['footer_menu'] = new TimberMenu();
+//		$context['footer_menu'] = new TimberMenu(3);
 //		$context['options'] = get_fields('option');
 		$context['site'] = $this;
 		return $context;
